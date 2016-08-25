4292ed070aefe1c9dac0ca94dc7d66c462cd9c35
Michael Q <MichaelQuiban@outlook.com>
2016-08-24 23:47:37 -0700

    Added Javascript
    Added object that contains questions and true or false answers.
    Excited to put this to work!

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log("hello world!");
+$(document).ready(function(){
+var currentQuestion = 0;
+var codeQuestions = [{
+    q: "Code is a set of instructions (or rules) that computers can understand; it might be helpful to think of code as a recipe.",
+    choices: ["True", "False"],
+    correct: 1
+  }, {
+    q: "People write code, code powers computers and computers power many everyday objects like phones, watches, microwaves and cars.",
+    choices: ["True", "False"],
+    correct: 1
+  }, {
+    q: "In fact, almost anything powered by electricity uses code.",
+    choices: ["True", "False"],
+    correct: 1
+  }, {
+    q: "There are many names for people who code: coders, programmers, developers, computer scientists, software engineers, etc.",
+    choices: ["True", "False"],
+    correct: 1
+  }, {
+    q: "",
+    choices: ["True", "False"],
+    correct: 1
+  }, {
+    q: "",
+    choices: ["Doctor Who", "The Office", "Jackie Chan Adventures", "이나정"],
+    correct: 3
+  }];
